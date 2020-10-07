3c84e557f768d8838012a152f077a95dca7bfe04
Katy Rog <katkopikat@gmail.com>
2020-10-07 03:37:57 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let pairBraickets = bracketsConfig.map((item) => item.join(''));
+    if (str.length % 2 !== 0) return false;
+
+    for (let i = 0; i < pairBraickets.length; i++) {
+        while (str.includes(pairBraickets[i])) {
+           str = str.replace(pairBraickets[i], '');
+           i = -1;
+        }
+    }
+    return str === '' ? true: false;
 }
